dd6e2f54a9a6772e7a39114c9a121eb56fa6acb1
Prajapatishivam65 <sp488830@gmail.com>
2025-06-11 16:29:30 +0530

    Added Global common folder of types and initialize the hub

--- apps/hub/index.ts
+++ apps/hub/index.ts
@@ -1,5 +1,20 @@
 import { randomUUIDv7, type ServerWebSocket } from "bun";
-
+import type { IncomingMessage, SignupIncomingMessage } from "common/types";
+import { prismaClient } from "db/client";
 import { PublicKey } from "@solana/web3.js";
 import nacl from "tweetnacl";
 import nacl_util from "tweetnacl-util";
+
+const availableValidators: {
+  validatorId: string;
+  socket: ServerWebSocket<unknown>;
+  publicKey: string;
+}[] = [];
+
+const CALLBACKS: { [callbackId: string]: (data: IncomingMessage) => void } = {};
+const COST_PER_VALIDATION = 100;
+
+async function signupHandler(
+  ws: ServerWebSocket<unknown>,
+  { ip, publicKey, signedMessage, callbackId }: SignupIncomingMessage
+) {}
